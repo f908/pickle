c27e373d1901dfd86194b05cf502b3543a32d3fe
Ogórki Gazowane <48841429+f908@users.noreply.github.com>
2022-04-19 20:48:13 +0200

    Pickle v0.0.2-r2

---
+++ komendy/poranny.js
@@ -0,0 +1,4 @@
+module.exports ={
+    name:"poranny",
+    code:`$channelSendMessage[$channelID;https://cdn.discordapp.com/attachments/966009218941468712/966047041845358622/poranny.png;false]`
+    }
