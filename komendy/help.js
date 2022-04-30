4d41e0b278c84fef6ba74438d29f7f83085c6222
Ogórki Gazowane <48841429+f908@users.noreply.github.com>
2022-04-30 20:36:24 +0200

    Pickle v0.0.3

--- komendy/help.js
+++ komendy/help.js
@@ -1,5 +1,4 @@
 module.exports ={
     name:"help",
-    code:`$channelSendMessage[$channelID;pickle slowko, pickle test, pickle help, pickle 2137, pickle かわいい, pickle awatar, pickle burrito, pickle liso, pickle poranny;false]
-$channelSendMessage[$channelID;Użycie CPU: $cpu% Użycie RAM: $ramMB, Wersja Node: $nodeVersion;false]`
-    }
+    code:`$channelSendMessage[$channelID;pickle slowko, pickle test, pickle help, pickle 2137, pickle かわいい, pickle awatar, pickle liso, pickle poranny, pickle kotki, pickle napisz, ~~pickle eval~~;false]
+$channelSendMessage[$channelID;Użycie CPU: $cpu% Użycie RAM: $ramMB, Wersja Node: $nodeVersi
---
+++ komendy/napisz.js
@@ -0,0 +1,4 @@
+module.exports ={
+    name:"napisz",
+    code:`$channelSendMessage[$channelID;$message;false]`
+    }
