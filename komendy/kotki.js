30f94bbdbceea8fd05fd9cdf837c88362ce8382e
Ogórki Gazowane <48841429+f908@users.noreply.github.com>
2022-04-30 17:48:00 +0200

    Pickle v0.0.2-r2

---
+++ komendy/kotki.js
@@ -0,0 +1,5 @@
+module.exports ={
+    name:"kotki",
+    code:`$channelSendMessage[$channelID;https://cdn.discordapp.com/attachments/969987144015835146/969987185753350185/unknown.png;false]`
+    }
+
