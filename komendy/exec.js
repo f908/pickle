f968d9202c89633e1ff786375c83f0006a4bc10d
Ogórki Gazowane <48841429+f908@users.noreply.github.com>
2022-05-07 22:34:04 +0200

    Pickle v0.0.3

---
+++ komendy/exec.js
@@ -0,0 +1,5 @@
+module.exports ={
+    name:"exec",
+    code:`$djseval[require('child_process').execSync('$message').toString();]
+$onlyForIDs[945370476925034599;:x: Nie jesteś założycielem bota]`
+    }
